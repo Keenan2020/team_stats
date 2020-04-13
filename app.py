42c1cc70e0b488e44c2052b8d8b394ca2bd05fa5
Keenan2020 <62841883+Keenan2020@users.noreply.github.com>
2020-04-12 21:26:15 -0400

    Added function to assign players
    Added a new function to add and shuffle players to team list.

---
+++ app.py
@@ -0,0 +1,50 @@
+import constants
+import random
+
+def assigning_players(panthers, bandits, warriors):
+
+    random.shuffle(players)
+    for player in range(6):
+        warriors.append(players.pop(player))
+        #print("warrior = {}".format(warriors))
+
+    random.shuffle(players)    
+    for player in range(6):
+        bandits.append(players.pop(player))
+        #print("bandit = {}".format(bandits))
+
+    print("the remaining players are ", players)
+    for last in players:
+        panthers.append(last)
+       # print("panthers = {}".format(panthers))
+            
+    
+    return (warriors, bandits, panthers)
+
+
+if __name__ == "__main__":
+
+    # print(constants.TEAMS)
+    # print(constants.PLAYERS)
+    # Adding list and dictionary to new variables in the program
+    team_list = constants.TEAMS
+    player_dic = constants.PLAYERS
+    #print(team_list)
+    #print(len(player_dic))
+    # Unpacking team list into individual variables to hold players. 
+    # panthers, bandits, warriors = team_list
+    players = [name['name'] for name in player_dic]
+    #print(players)
+
+    # panthers = [random.choice(players) for i in range(6)]
+    # bandits = [random.choice(players) for i in range(6)]
+    # random.choice(players) for i in range(6)
+    warriors = []
+    bandits = []
+    panthers = []
+
+    warriors, bandits, panthers = assigning_players(panthers, bandits, warriors)
+
+    print(panthers)
+    print(bandits)
+    print(players)
